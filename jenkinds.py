e5961c4fdf62c045e8bed02c40d029a306a788f0
xiaofeng68 <xiao_feng68@163.com>
2020-12-13 09:11:10 +0800

    增加多线程下载

--- jenkinds.py
+++ jenkinds.py
@@ -1,6 +1,8 @@
 import json
 import requests
 import os
+from concurrent.futures import ThreadPoolExecutor, wait, ALL_COMPLETED
+
 url = 'https://mirrors.tuna.tsinghua.edu.cn/jenkins/'
 # Python 字典类型转换为 JSON 对象
 
@@ -15,11 +17,18 @@ def downloadPlugin(plugin):
 	with open('%s/%s.hpi'%(path,plugin['name']), "wb") as code:
 		code.write(down_res.content)
 with open('update-center.json','r',encoding='utf-8') as f:
+	urls = []
 	for line in f.readlines():
 		data = json.loads(line)
 		plugins = data['plugins']
 		for i in plugins:
-			downloadPlugin(plugins[i])
+			#downloadPlugin(plugins[i])
+			urls.append(plugins[i])
 			depends = plugins[i]['dependencies']
 			for j in range(len(depends)):
-				downloadPlugin(depends[j])
+				# downloadPlugin(depends[j])
+				urls.append(depends[j])
+	executor = ThreadPoolExecutor(len(urls))
+	future_tasks = [executor.submit(downloadPlugin, plugin) for plugin in urls]
+	wait(future_tasks, return_when=ALL_COMPLETED)
+
